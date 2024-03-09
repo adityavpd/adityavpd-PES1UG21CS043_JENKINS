e718f8d2d007328c62d3905736681b41db72ecb6
adityavpd <aditya.dvp@gmail.com>
2024-03-09 20:02:51 +0530

    #56

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Webhooks Jenkins from Aditya!!!\n";
+  cout << "Hello, Webhooks Jenkins from Aditya D Venkatesh\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   return 0;
