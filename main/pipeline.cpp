ca62089b3a182398c5144bef07f3476e6844b0af
adityavpd <65660533+adityavpd@users.noreply.github.com>
2024-03-09 20:20:19 +0530

    Create pipeline.cpp

---
+++ main/pipeline.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Pipeline by Aditya D\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
